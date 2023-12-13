cfbec9c64e4651409177521ae3f322162f2c10ef
Hoon Choi <hoon.choi.kr@gmail.com>
2023-12-13 13:34:16 +0000

    Delivery query added

---
+++ query_delivery.py
@@ -0,0 +1,42 @@
+# Additional Query
+# When delivery is completed, move the order to past order and change relevant status
+# Input: order_id
+# Output: Bool indicating job status
+
+# Flow
+# take order id
+# get order information from order id
+# change 'status' to 'Delivered'
+# Move the order to past_order collection
+# change on_delivery from 1 to 0
+# remove order_id from the partner
+
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = MongoClient('mongodb+srv://hoon:hoon@amazone.4sbahbn.mongodb.net/')
+db = client['amazone']
+
+def change_status_and_move_order(db, order_id):
+    db['current_orders'].update_one({'_id': order_id}, {'$set': {'status': 'Delivered'}})
+    order = db['current_orders'].find_one({'_id': order_id})
+    db['past_orders'].insert_one(order)
+    db['current_orders'].delete_one({'_id': order_id})
+
+def update_partner_info(db, order_id):
+    partner = db['partners'].find_one({'order_id': order_id})
+    partner['on_delivery'] = 0
+    partner['order_id'] = None
+
+def process_order(order_id):
+
+    if change_status_and_move_order(db, order_id):
+        update_partner_info(db, order_id)
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    order_id = ObjectId("656dbed02729ed87da90697e")
+    result = process_order(order_id)
+    print("Operation successful:", result)
