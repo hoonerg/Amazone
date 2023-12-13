72adb23ef22712f72eddb3bd515ddaa884ed5b44
Hoon Choi <hoon.choi.kr@gmail.com>
2023-12-13 13:41:17 +0000

    Added query for getting top 5 result

---
+++ top_5_result.py
@@ -0,0 +1,34 @@
+# Requires the PyMongo package.
+# https://api.mongodb.com/python/current
+client = MongoClient('mongodb+srv://sailor:salaar@amazone.4sbahbn.mongodb.net/')
+result = client['amazone']['past_orders'].aggregate([
+    {
+        '$unwind': '$items'
+    }, {
+        '$lookup': {
+            'from': 'products', 
+            'localField': 'items.product_id', 
+            'foreignField': '_id', 
+            'as': 'product'
+        }
+    }, {
+        '$unwind': '$product'
+    }, {
+        '$project': {
+            'product_name': '$product.product_name', 
+            'quantity': '$items.quantity', 
+            'price': '$product.standard_price_to_customers', 
+            'total_amount': {
+                '$multiply': [
+                    '$items.quantity', '$product.standard_price_to_customers'
+                ]
+            }
+        }
+    }, {
+        '$sort': {
+            'total_amount': -1
+        }
+    }, {
+        '$limit': 5
+    }
+])
